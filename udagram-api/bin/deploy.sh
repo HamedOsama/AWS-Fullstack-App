291a78f0002777c06983a95f7d10c78e9dfcbd86
Hamed Osama <hamedosama19@gmail.com>
2022-06-02 14:01:36 +0200

    edited

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
-eb init udagram-api-dev1 --platform node.js --region us-east-1 
-eb use udagram-api-dev1 --profile default
+eb init udagram-api --platform node.js --region us-west-2 
+eb use udagram-api-dev --profile default
 eb deploy --profile default 
 
 
